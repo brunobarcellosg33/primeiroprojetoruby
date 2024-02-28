965257a7efa913158107ebf36df33a6f76b9730c
Bruno Giovanni Barcellos <107158560+brunobarcellosg33@users.noreply.github.com>
2024-02-27 23:01:38 -0300

    Create aula3.rb

---
+++ aula3.rb
@@ -0,0 +1,13 @@
+print "Digite seu nome: "
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Diga sua idade: "
+
+idade = gets.chomp
+
+puts "Lhes apresento #{nome} #{sobrenome}, de #{idade} anos!"
